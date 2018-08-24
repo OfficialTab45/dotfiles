ff71da58f54edb23b5a44c81d493551745f2d4ba
Tab <37502375+TheTab@users.noreply.github.com>
2018-08-25 00:59:00 +0200

    Update zfs-update.sh

--- zfs-update.sh
+++ zfs-update.sh
@@ -14,3 +14,22 @@ $ZFS rename zroot/ROOT/STABLE zroot/ROOT/OLDSTABLE
 $CP /boot/vm-linuz-linux-stable /boot/vmlinuz-linux-oldstable
 $CP /boot/initramfs-linux-stable.img /boot/initramfs-linux-oldstable.img
 
+##Stable
+ZFS snapshot zroot/ROOT/default@stable
+$ZFS clone zroot/ROOT/default@stable zroot/ROOT/STABLE
+$ZFS set canmount=noauto zroot/ROOT/STABLE
+$ZFS set mountpoint=/ zroot/ROOT/STABLE
+$CP /boot/vmlinuz-linux /boot/vmlinuz-linux-stable
+$CP /boot/initramfs-linux.img /boot/initramfs/linux-stable.img
+
+##Update System
+
+$REFLC -l 5 -p https --sort rate --save /etc/pacman.d/mirrorlist
+$PAC -Syu
+
+#Log
+
+$ZFS snapshot zroot/ROOT/default@upgrade
+$DATE >> var/log/upgrade/upgrade.log
+$ZFS diff zroot/ROOT/default@stable zroot/ROOT/default@upgrade >> /varr/log/upgrade/upgrade.log
+$ZFS destroy zroot/ROOT/default@upgrade
