d8c23726dae2444c3565ac246b6dcb5108a60581
Tab <37502375+TheTab@users.noreply.github.com>
2018-08-25 00:53:31 +0200

    Update zfs-update.sh

--- zfs-update.sh
+++ zfs-update.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
+ZFS=/usr/bin/zfs
+ZPOOL=/usr/bin/zpool
+RM=/usr/bin/rm
+CP=/usr/bin/cp
+REFLC=/usr/bin/reflector
+PAC=/usr/bin/pacman
+
+##Old Stable
+
+$ZFS destroy -R zroot/ROOT/default@oldstable
+$ZFS rename zroot/ROOT/default@stable zroot/ROOT/default@oldstable
+$ZFS rename zroot/ROOT/STABLE zroot/ROOT/OLDSTABLE
+$CP /boot/vm-linuz-linux-stable /boot/vmlinuz-linux-oldstable
+$CP /boot/initramfs-linux-stable.img /boot/initramfs-linux-oldstable.img
 
